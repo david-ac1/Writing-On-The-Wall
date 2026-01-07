ec0be98a313d2278f727b434eb72e55ca756baf1
Ebuka <davidachibiri8@gmail.com>
2026-01-08 00:33:08 +0200

    Add webpack alias to ignore non-existent react-pdf CSS files

--- next.config.ts
+++ next.config.ts
@@ -10,6 +10,15 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  webpack: (config) => {
+    // Ignore react-pdf CSS imports that don't exist
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      'react-pdf/dist/esm/Page/AnnotationLayer.css': false,
+      'react-pdf/dist/esm/Page/TextLayer.css': false,
+    };
+    return config;
+  },
 };
 
 export default nextConfig;
